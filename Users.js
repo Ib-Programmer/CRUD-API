2cb7bb4dce32a8907b20c99c53f2f573426934f6
Ibrahim Mohammed <36298932+Ib-Programmer@users.noreply.github.com>
2022-10-02 11:08:38 +0100

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,23 @@
+const users = [
+    {
+        id:1,
+        name:'Ibrahim',
+        email:'ibrahim@gmail.com'
+    },
+    {
+        id:2,
+        name:'Musa',
+        email:'musa@gmail.com'
+    },
+    {
+        id:3,
+        name:'Usman',
+        email:'usman@gmail.com'
+    },
+    {
+        id:4,
+        name:'Sani',
+        email:'Sani@gmail.com'
+    }
+]
+module.exports = users;
